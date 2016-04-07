b8d8249a212f6547b8c5ebdd8e1b9e813d7df216
Francisco Gileno <gileno@gmail.com>
2016-04-07 12:28:57 -0300

    Fixing little issues

--- cards/app.jsx
+++ cards/app.jsx
@@ -1,1 +1,18 @@
-//Your components go here
+//*********************
+//*** Code Examples ***
+//*********************
+// $.getJSON('/cards/data/colaboradores.json', function(data) {
+//   //setState
+// });
+//
+// var App = React.createClass({
+//   render: function() {
+//     return (
+//       //Your HTML here
+//     );
+//   },
+// });
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+//
